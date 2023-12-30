9bab5b077d9f2faf1fc2814edb728976d4af02c4
sisah <jirielbunda@seznam.cz>
2023-12-30 13:27:22 -0500

    ndk r26

--- buildscripts/include/download-ndk.sh
+++ buildscripts/include/download-ndk.sh
@@ -15,11 +15,4 @@ fi
 
 curl --http1.1 "https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip" > $NDK_FILE
 
-echo "==> Checking NDK zip file integrity"
-FILE_HASH=$(sha256sum $NDK_FILE | awk '{print $1}' )
-if [[ $FILE_HASH != $NDK_HASH ]]; then
-	echo "Failed, expected $NDK_HASH got $FILE_HASH"
-	exit 1
-fi
-
 popd
